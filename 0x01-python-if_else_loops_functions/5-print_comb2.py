2552f4b4a80f56a381c616c98ddccebfed3c0b72
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2023-11-28 23:47:00 +0200

    Fix issue

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for i in range(0, 100):
     if i == 99:
-        print("{:02}".format(i))
+        print(f"{i:02}")
     else:
-        print("{:02}, ".format(i), end="")
+        print(f"{i:02}, ", end="")
