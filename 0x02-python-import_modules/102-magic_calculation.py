57061022f0e5d4bdaece147e21a4a227802eed7e
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2023-12-01 00:20:04 +0200

    Add soution

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,10 +4,13 @@ import dis
 def magic_calculation(a, b):
     '''magic_calculation
     '''
-    from magic_calculation_102 import add as add, sub as sub
-    
+    from magic_calculation_102 import add, sub
+
     if a < b:
         c = add(a, b)
+        for n in range(4, 6):
+            c = add(c, n)
+        return c
     else:
         return sub(a, b)
 
