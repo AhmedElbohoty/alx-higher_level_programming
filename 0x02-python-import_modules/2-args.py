aa8d38c2f78ffd6000ac8e0461429212dea343cb
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2023-11-30 22:46:21 +0200

    Add solution for 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+
+count = len(sys.argv) - 1
+first_str = "{:d} "
+if count == 1:
+    first_str += "argument"
+else:
+    first_str += "arguments"
+
+if count == 0:
+    first_str += "."
+else:
+    first_str += ":"
+
+print(first_str.format(count))
+
+index = 0
+for arg in sys.argv:
+    if index != 0:
+        print("{:d}: {:s}".format(index, arg))
+    index += 1
