7df8d42d0d71939dd69a0b9719e0ddd2e4cb57f2
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2023-12-03 21:47:41 +0200

    Add soluton for 2-replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    '''Replaces an element of a list at a specific position (like in C).
+    '''
+    if idx < 0:
+        return None
+
+    if idx >= len(my_list):
+        return None
+
+    my_list[idx] = element
+
+    return my_list
