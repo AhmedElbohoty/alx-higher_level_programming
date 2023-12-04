252bec2c02084f6d90eae5051920ff596f8ea6fe
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2023-12-04 18:24:31 +0200

    Fix issue

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -21,8 +21,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-int is_palindrome_helper(listint_t *head, int start, int end);
+int is_palindrome_helper(listint_t *head, int end);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 size_t listint_len(const listint_t *h);
+int pop_listint(listint_t *head);
 
 #endif /* LISTS_H */
