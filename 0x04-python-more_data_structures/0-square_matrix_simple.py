36818570fd3ac696c8ce1d46fe4be2e0f0ade308
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2023-12-06 22:43:38 +0200

    Add comments

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
+    '''Computes the square value of all integers of a matrix.
+    '''
     return [[num * num for num in row] for row in matrix]
