b176de780750f3b83af54e77f6f0dde689ba8017
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2023-12-18 21:41:40 +0200

    Fix issue

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,13 +2,13 @@
 def safe_print_list(my_list=[], x=0):
     '''Prints x elements of a list.
     '''
-    len = 0
+    c = 0
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
         except Exception:
             break
         else:
-            len += 1
+            c += 1
     print("")
-    return len
+    return c
