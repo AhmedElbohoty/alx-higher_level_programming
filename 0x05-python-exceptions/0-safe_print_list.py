0fe34f639c21d3e4bef2aae345f18e578ca6c3b7
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2023-12-18 21:38:46 +0200

    Fix issue

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,9 +10,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end=end)
             i += 1
         except Exception:
-            break
-
-        finally:
             print("")
+            break
 
     return i
