e8ee60b7bb036856856cb374748b54830a49f539
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2023-12-19 22:06:53 +0200

    Add solution for 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Create a class Square"""
+
+
+class Square:
+    """Used to create a square.
+    """
+    def __init__(self, size=0):
+        """Create new instance
+
+        Args:
+            size: size of one side of square
+
+        Attributes:
+            size: size of one side of square
+        """
+        self.__size = size
+
+    def area(self):
+        """Returns the square area
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Get the one side size of square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Size setter
+
+        Args:
+            size (int): size of one side.
+
+        Raises:
+            ValueError: size must be >= 0
+            TypeError: size must be an integer
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def my_print(self):
+        """Prints in stdout the square with the character #.
+        """
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(0, self.__size):
+                print("#" * (self.__size))
