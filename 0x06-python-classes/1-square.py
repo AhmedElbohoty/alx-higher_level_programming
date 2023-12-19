dce0b84b4c635647a23c34cb9bc22197f0950656
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2023-12-19 20:40:15 +0200

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Create a class Square"""
+
+
+class Square:
+    """Used to create a square.
+    """
+    def __init__(self, size):
+        """Create new instance
+
+        Args:
+            size: size of one side of square
+
+        Attributes:
+            size: size of one side of square
+        """
+        self.__size = size
