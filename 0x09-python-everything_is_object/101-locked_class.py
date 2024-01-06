f0a520564155463a5295753d2bdeb86fb5078aa6
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-01-06 23:14:16 +0200

    Fix issue

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Define class locked to specific attributes names'''
+
+
+class LockedClass:
+    '''Define class locked to specific attributes names'''
+    __slots__ = 'first_name'
