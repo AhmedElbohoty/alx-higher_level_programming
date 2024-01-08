243ec9a4aae6e663e4875dd7c0ff13f25cfcce22
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-01-08 23:14:33 +0200

    Add solution for 1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+'''Create a class MyList that inherits from list.
+
+Functions:
+    print_sorted
+'''
+
+
+class MyList(list):
+    '''Class that inherits from list
+    '''
+
+    def print_sorted(self):
+        '''Prints the list, but sorted (ascending sort).
+        '''
+        lst = self.copy()
+        lst.sort()
+        print(lst)
