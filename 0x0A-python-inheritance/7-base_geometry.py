f331d1ceac6cd519c270daf3670c2c73cf3b41d4
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-01-09 00:48:35 +0200

    Add solution for 7-base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''BaseGeometry'''
+
+
+class BaseGeometry():
+    '''BaseGeometry class'''
+
+    def area(self):
+        '''Public instance method - Not implemented.'''
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        '''Validate the value
+
+        Args:
+            name (str): the name
+            value (int): integer greater than 0
+
+        Raises:
+            - TypeError: if value is not an integer
+                         ('<name> must be an integer')
+            - ValueError: if value is less or equal to 0
+                          ('<name> must be greater than 0')
+        '''
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/7-base_geometry.txt")
