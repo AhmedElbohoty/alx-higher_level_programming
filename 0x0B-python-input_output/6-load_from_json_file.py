a5d24761104da22722ddebb1767ebd88a6902a45
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-01-10 00:37:16 +0200

    Add solution for 7-add_item.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-'''Creates an Object from a “JSON file”:
+'''Creates an Object from a “JSON file”
 '''
 import json
 
 
 def load_from_json_file(filename):
-    '''Creates an Object from a “JSON file”:
+    '''Creates an Object from a “JSON file”
 
     Args:
         filename (str): the JSON filename
