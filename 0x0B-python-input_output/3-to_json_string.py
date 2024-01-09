d9bdea5b271e027405eacc87a172510f7f405bb6
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-01-09 23:42:36 +0200

    Add solution for 3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Create the JSON representation of an object (string)
+'''
+import json
+
+
+def to_json_string(my_obj):
+    '''Create the JSON representation of an object (string)
+
+    Args:
+        my_obj (any): the object to be serialized
+
+    Returns:
+        str: The JSON representation of the object.
+    '''
+    return json.dumps(my_obj)
