f61e0008954b7e3030317643bf623da702e9575c
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-01-10 01:54:48 +0200

    Add solution

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -14,7 +14,7 @@ def append_after(filename='', search_string='', new_string=''):
         new_string (str): The string to append.
     '''
     text = ''
-    with open(filename, 'r+', encoding='utf-8') as file_obj:
+    with open(filename, 'w+', encoding='utf-8') as file_obj:
         for line in file_obj:
             text += line
             if search_string in line:
