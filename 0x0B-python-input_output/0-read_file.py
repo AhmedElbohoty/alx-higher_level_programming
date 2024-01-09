d89961ab80d8aaf3080f7f7b88e8b9bebdb5da12
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-01-09 23:26:00 +0200

    Add solution for 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Reads a text file (UTF8) and prints it to stdout'''
+
+
+def read_file(filename=""):
+    '''Reads a text file (UTF8) and prints it to stdout
+
+    Args:
+        filename (str optional): the file name
+    '''
+
+    if not filename:
+        return
+
+    with open(filename, 'r', encoding="utf-8") as file_obj:
+        for line in file_obj:
+            print(line, end='')
+        print()
