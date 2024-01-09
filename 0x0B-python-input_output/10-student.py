a49b39c7e9e1ffeababf200f500a982db4d81ea8
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-01-10 01:20:10 +0200

    Add solution

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+'''Student class'''
+
+
+class Student:
+    '''Class Student that defines a student props and methods
+    '''
+
+    def __init__(self, first_name, last_name, age):
+        '''Init the instance.
+
+        Args:
+            first_name (str): the first name.
+            last_name (int): the last name.
+            age (int): the age.
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''Returns a dictionary representation.
+
+        Args:
+            attrs (list of strings): the list of strings.
+
+        Returns:
+            dictionary representation.
+        '''
+        if type(attrs) != list:
+            return self.__dict__
+
+        ndict = {}
+        for name in attrs:
+            if type(name) != str:
+                return self.__dict__
+
+            if hasattr(self, name):
+                ndict[name] = self.__dict__[name]
+
+        return ndict
