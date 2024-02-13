06b546d0a5ffb7bc5240dbac757a977ed7e82a98
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-02-14 00:58:35 +0200

    Add more solutions

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- Creates the database hbtn_0c_0 in your MySQL server.
+
+-- Create database called hbtn_0c_0
+CREATE database IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- Deletes the database hbtn_0c_0 in your MySQL server.
+
+-- Deletes the database
+DROP database IF EXISTS hbtn_0c_0;
