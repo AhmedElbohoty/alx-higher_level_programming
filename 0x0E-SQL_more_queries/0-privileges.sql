9c1e13559900b27367a3b6e16478da0d5618bcb4
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-02-14 23:03:55 +0200

    Add solution

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,6 @@
 /* Lists all privileges of the MySQL users user_0d_1 and user_0d_2 */
-SHOW GRANTS FOR user_0d_1@localhost, user_0d_2@localhost;
+
+/* Show all privileges for user_0d_1 */
+SHOW GRANTS FOR user_0d_1@localhost;
+/* Show all privileges for user_0d_2 */
+SHOW GRANTS FOR user_0d_2@localhost;
