b0a0dc0c68ab65cad1129790c80d790a5d00cd43
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-02-14 12:31:48 +0200

    Add more solutions

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,10 +3,13 @@
 
 
 -- Database hbtn_0c_0
-ALTER DATABASE hbtn_0c_0 IF EXISTS CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0 ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Table first_table
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- ALTER DATABASE hbtn_0c_0 IF EXISTS CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Field name in first_table
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- -- Table first_table
+-- ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- -- Field name in first_table
+-- ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
