7a754f02c42243faf4f680ee018d7dcaa6cdc869
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-02-14 23:56:55 +0200

    Add solution

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,11 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2.
 
 -- Create new database called hbtn_0d_2
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 
 -- Create a new user called user_0d_2.
-CREATE USER IF NOT EXISTS user_0d_2
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Add privilege to the user
+GRANT SELECT ON 'hbtn_0d_2' TO 'user_0d_2'@'localhost';
