a22c3864be5a6dd335a49ed775b5887f60071aaf
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-02-15 00:51:01 +0200

    Add solution

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,5 +3,5 @@
 
 SELECT c.id, c.name
   FROM cities AS c, states AS s
-  WHERE s.name = 'California' AND c.state_id = s.id
-  ORDER BY c.id;
+ WHERE s.name = 'California' AND c.state_id = s.id
+ ORDER BY c.id;
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,1 @@
+-- Lists all cities contained in the database hbtn_0d_usa.
