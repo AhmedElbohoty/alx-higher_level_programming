65aca198d9ce03ce4bf8d86233238a21a9010606
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-02-14 23:58:36 +0200

    Add solution

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2.
 
 -- Create new database called hbtn_0d_2
-CREATE database IF NOT EXISTS 'hbtn_0d_2';
+CREATE database IF NOT EXISTS hbtn_0d_2;
 
 -- Create a new user called user_0d_2.
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
