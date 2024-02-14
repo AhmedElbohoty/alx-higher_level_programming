c265fdcac4c0ca0c527727df359fdbd6b2841769
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-02-14 12:38:27 +0200

    Add more solutions

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,14 +2,16 @@
 -- in your MySQL server. You need to convert all of the following to UTF8:
 
 
+USE hbtn_0c_0 
+
 -- Database hbtn_0c_0
-USE hbtn_0c_0 ALTER TABLE first_table
+ALTER DATABASE hbtn_0c_0
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- ALTER DATABASE hbtn_0c_0 IF EXISTS CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Table first_table
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- -- Table first_table
--- ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- -- Field name in first_table
--- ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Field name in first_table
+ALTER TABLE first_table MODIFY name VARCHAR(256) 
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
