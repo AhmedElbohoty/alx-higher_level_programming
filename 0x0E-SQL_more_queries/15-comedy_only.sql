9e80745f67410cdd4de9592c6fd0133483c60c2d
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-02-15 02:48:40 +0200

    Add solution

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -5,5 +5,5 @@ SELECT tv_shows.title
     ON tv_genres.id = tv_show_genres.genre_id
  INNER JOIN tv_shows
     ON tv_shows.id = tv_show_genres.show_id
- WHERE tv_genres.name = 'Comedy';
+ WHERE tv_genres.name = 'Comedy'
   ORDER BY tv_shows.title;
