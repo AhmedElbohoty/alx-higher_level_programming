dd7833985cf789c47d9121e3e92cfbb8bf530b73
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-03-12 17:02:28 +0200

    Add solution

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const { list } = require('./100-data');
+const list = require('./100-data').list;
 const multList = list.map((e, ind) => e * ind);
 console.log(list);
 console.log(multList);
