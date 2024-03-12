6e0fbd23bd80726cd719042a26bfa68d621dd6a5
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-03-12 17:27:15 +0200

    Add solution

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,17 +2,16 @@
 const fs = require('fs');
 const args = process.argv;
 
+const file1 = readFile(args[2]);
+const file2 = readFile(args[3]);
+
 function readFile (path) {
   return fs.readFileSync(path, 'utf-8');
 }
 
-function writeToFile (file) {
-  fs.appendFile(file3, file + '\n', (err) => err);
+function writeToFile (data) {
+  fs.appendFile(args[4], data + '\n', (err) => err);
 }
-
-const file1 = readFile(args[2]);
-const file2 = readFile(args[3]);
-const file3 = readFile(args[4]);
 
 writeToFile(file1);
 writeToFile(file2);
