f8bb8b197dcb2b384c1d1bb2c2db9ee09703fc0c
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-03-17 01:18:27 +0200

    Add solution

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+'''Script that takes in an argument and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument.'''
+import sys
+import MySQLdb
+
+
+def display_states_name(username, password, database_name, state_name):
+    '''
+    List all states from the specified MySQL database hbtn_0e_0_usa.
+
+    Args:
+        username (str): The user name.
+        password (str): The password.
+        database_name (str): The database name.
+        state_name (str): The state name.
+
+    Returns:
+        None
+    '''
+    connection = MySQLdb.connect(host='localhost', user=username,
+                                 passwd=password, db=database_name,
+                                 port=3306)
+
+    cursor = connection.cursor()
+
+    cursor.execute(
+        "SELECT * FROM `states` WHERE BINARY `name` = `{}`".format(state_name))
+
+    states = cursor.fetchall()
+
+    for state in states:
+        if state[1][0] == "N":
+            print(state)
+
+    # Clear up the cursor and connection
+    cursor.close()
+    connection.close()
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    display_states_name(args[1], args[2], args[3], args[4])
