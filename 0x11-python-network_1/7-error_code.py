16dfb393d770486a35c24f59bfcaf0670ec36047
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-03-28 13:54:46 +0200

    Add solution

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 ''' Python script that takes in a URL, sends a request to the URL
 and displays the body of the response. '''
-import requests
 import sys
+import requests
 
 if __name__ == '__main__':
     argv = sys.argv
